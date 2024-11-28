88d0d957da671a08df1fcde1320b46ab46b1ad00
Luiz Campos <campos.antoonio@gmail.com>
2024-11-28 09:54:50 -0300

    feat: allowing sheet to receive a container
    The container can be used to choose where you want to display a element
    with portal.
    
    usage:
        <SheetContent container={document.getElement...} ...>

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -49,13 +49,15 @@ const sheetVariants = cva(
 
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-    VariantProps<typeof sheetVariants> {}
+    VariantProps<typeof sheetVariants> {
+  container?: Element | DocumentFragment | null | undefined;
+}
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, ...props }, ref) => (
-  <SheetPortal>
+>(({ side = "right", container, className, children, ...props }, ref) => (
+  <SheetPortal container={container}>
     <SheetOverlay />
     <SheetPrimitive.Content
       ref={ref}
