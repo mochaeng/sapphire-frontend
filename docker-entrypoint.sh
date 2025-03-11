8e3f48c4d0a76059cb2d1dfc31d79364a596e454
Luiz Campos <campos.antoonio@gmail.com>
2025-03-11 01:41:06 -0300

    fix: enable set media url at runtime

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,5 @@
 
 # replace the placeholder with the runtime value
 find /usr/share/nginx/html -type f -exec sed -i "s|__RUNTIME_API_URL__|$RUNTIME_API_URL|g" {} +
+find /usr/share/nginx/html -type f -exec sed -i "s|__RUNTIME_STATIC_URL__|$RUNTIME_STATIC_URL|g" {} +
 exec "$@"
